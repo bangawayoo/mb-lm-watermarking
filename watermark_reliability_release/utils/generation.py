523ca8a26b66cfda25491dc878186d2583bfd903
bangawayoo <user@AL01994450.local>
2023-08-01 14:45:05 +0900

    edit on shell

--- watermark_reliability_release/utils/generation.py
+++ watermark_reliability_release/utils/generation.py
@@ -53,13 +53,16 @@ def load_model(args):
     if args.is_seq2seq_model:
         model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name_or_path)
     elif args.is_decoder_only_model:
-        model = LlamaForCausalLM(args.model_name_or_path)
-        # if args.load_fp16:
-        #     model = AutoModelForCausalLM.from_pretrained(
-        #         args.model_name_or_path, torch_dtype=torch.float16, device_map="auto"
-        #     )
-        # else:
-        #     model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path)
+        if "llama" in args.model_name_or_path:
+            model = LlamaForCausalLM.from_pretrained(args.model_name_or_path,
+                                                     torch_dtype=torch.float16)
+        else:
+            if args.load_fp16:
+                model = AutoModelForCausalLM.from_pretrained(
+                    args.model_name_or_path, torch_dtype=torch.float16, device_map="auto"
+                )
+            else:
+                model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path)
     else:
         raise ValueError(f"Unknown model type: {args.model_name_or_path}")
 
